a70e5c66398a40b3402ba4ce8c68ca3e60322b04
HenryLai <engine.stack@gmail.com>
2024-02-26 00:06:59 +0800

    次標

--- src/components/FeatureSection.tsx
+++ src/components/FeatureSection.tsx
@@ -24,7 +24,6 @@ const FeatureSection: React.FC = () => {
     <section className={styles.featureSection} style={{ backgroundImage: `url(${images[activeIndex]})` }}>
       <div className={styles.content}>
         <h1 className={styles.contentTitle }>SDG 行動幣募資</h1>
-<<<<<<< HEAD
         <div className='flex'>
           <div className={styles.minted}>
             <span>
@@ -44,22 +43,6 @@ const FeatureSection: React.FC = () => {
           </Link> */}
         </div>
         {/* <p>目前兌換 1 ETH : 30 SDG</p> */}
-=======
-        <div className={styles.minted}><p>97 / 10,000,000  - SDG 發行</p>
-        </div><div className={styles.mtop10px}>
-        <button className={styles.button}>鑄幣贊助</button>
-        <button className={styles.button}>馬上提案</button>
-        </div> 
-        <div className={styles.mtop10px}><p>完成任務並蒐集NFT成為行動達人</p>
-        <Link href="/explore">
-            <div className={`${styles.link} cursor-pointer`}>了解更多</div>
-          </Link>
-        </div>
-        {/* <p>目前兌換 1 ETH : 30 SDG</p> */}
-                  
-         
-      
->>>>>>> df54ef3001cdddc4299176920638ff24b4b3a0c6
       </div>
     </section>
   );
